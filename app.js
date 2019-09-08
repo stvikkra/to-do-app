7f467db4f9794bd908a1aefa2aa4e0c4355be606
STVI <stvikkra@gmail.com>
2019-09-08 19:30:03 -0400

    Checkpoint Complete

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 
 
 function onReady() {
-   const toDos = [];
+   let toDos = [];
    const addToDoForm = document.getElementById('addToDoForm');
-
+   let id = 0;
    function createNewToDo() {
       const newToDoText = document.getElementById('newToDoText');
       if (!newToDoText.value) { return; }
 
       toDos.push({
         title: newToDoText.value,
-        complete: false
+        complete: false,
+        id: id
    });
+
+   id++;
+
     newToDoText.value = '';
 
     renderTheUI();
@@ -27,10 +31,22 @@ function onReady() {
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
 
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener('click', event =>{
+          toDos = toDos.filter(function(item){
+            return item.id !== toDo.id;
+          })
+
+          renderTheUI();
+        });
+
         newLi.textContent = toDo.title;
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
+        newLi.appendChild(deleteBtn);
 
    });
     }
@@ -41,6 +57,7 @@ function onReady() {
    createNewToDo();
  });
 
+}
  window.onload = function() {
    onReady();
  };
